b9eeffc53c93440417d0ef0aeee22ca86dffd8d8
Geoff Matheson <111482451+mrmatho@users.noreply.github.com>
2024-09-05 11:52:19 +0000

    Added AFL live scores.

--- my_training_bot.py
+++ my_training_bot.py
@@ -1,4 +1,5 @@
 import discord
+import requests
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -23,6 +24,28 @@ async def on_message(message):
     if message.content.startswith('$help'):
         await message.channel.send('Hello Legend! I am your training bot. I can help you with your training. Just type $hello, $bye or $help to get started.')
     
-    
+    if message.content.startswith('$afl') or message.content.startswith('$AFL'):
+        try:
+            header = {'User-Agent': "Cheeky Little Discord Bot - geoffmatheson@gmail.com"}
+            url = "https://api.squiggle.com.au/?q=games;live=1"
+            response = requests.get(url, headers = header)
+            #print(response.text)
+            data = response.json()
+            games = data['games']
+            if games:
+                for g in games:
+                    print(g)
+                    score = f"{g['hteam']} {g['hgoals']}.{g['hbehinds']}.{g['hscore']} vs {g['ateam']} {g['agoals']}.{g['abehinds']}.{g['ascore']}"
+                    msg = f"{g['roundname']} - {g['venue']}"
+                    updated = f"Updated: {g['updated']}"
+                    await message.channel.send(score)
+                    await message.channel.send(msg)
+                    await message.channel.send(updated)
+            else:
+                await message.channel.send('No games are currently live.')
+        except Exception as e:
+            print(e)
+            await message.channel.send('Sorry, I could not get the AFL scores at the moment.')
+        
 
 client.run('MTIzNDM1NTQwMTU2NzM3NTM4MA.GgmFAS.7lOYJBEkjA6zbSSARs_7LNP_9aiukdazf09EqU')
